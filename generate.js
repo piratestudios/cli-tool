5bf45b079c4da917dccd8c9a3fb1f0b79e8f05c9
Alvaro Torpedo <goodbytes23@yahoo.es>
2017-05-19 17:13:16 +0200

    Added action and changed names of files for convenience on the help description

--- generate.js
+++ generate.js
@@ -8,15 +8,21 @@ program.version(package.version)
 program.on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('    $ generate g <name-component>');
+    console.log('    $ generate c [name-component]');
     console.log('');
 });
 
+program
+    .command('c [name]')
+    .description('generate component named [name]')
+    .action(function (name) {
+        console.log('component %s ', name);
+    });
+
 program.parse(process.argv);
 
-if (!process.argv.slice(2).length) {
+if (!process.argv.slice(2).length)
     program.outputHelp();
-}
 
 if (!program.version)
     console.log(program.version())
