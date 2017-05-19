c33c4624510fa6d4e4e5638510b39044c295e3a2
Alvaro Torpedo <goodbytes23@yahoo.es>
2017-05-19 17:06:10 +0200

    Output help by default

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ program.version(package.version)
 program.on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('    $ generate --help');
+    console.log('    $ generate g <name-component>');
     console.log('');
 });
 
 program.parse(process.argv);
 
-// let args = process.argv.slice(2);
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
 
 if (!program.version)
     console.log(program.version())
