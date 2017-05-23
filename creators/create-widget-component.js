d8e48170fc314f80595411bd26c1c59d25b158ee
Alvaro Torpedo <goodbytes23@yahoo.es>
2017-05-23 15:04:01 +0200

    Generate and delete widgets

--- creators/create-widget-component.js
+++ creators/create-widget-component.js
@@ -5,8 +5,28 @@ module.exports = function (name) {
     console.log('Creating widget component %s...', name);
 
     const
+        indexTemplate = require('../templates/templateIndex.js')(name),
+        jsxTemplate = require('../templates/template.jsx')(name),
+        testTemplate = require('../templates/template.test.js')(name),
+        nameTemplate = require('../templates/template.js')(name),
         type = 'widgets',
         extensions = [];
+
+    extensions.push('.scss');
+    extensions.push('.css');
+    extensions.push('.test.js');
+    extensions.push('.jsx');
+    extensions.push('.js');
+
+    createDirectory(type);
+    createDirectory(type + "/" + name);
+    createFiles(extensions, name, type, 'internal data');
+    createFile('index.js', name, type, indexTemplate);
+    modifyFile('js', name, type, nameTemplate);
+    modifyFile('jsx', name, type, jsxTemplate);
+    modifyFile('test.js', name, type, testTemplate);
+    modifyLazyFileAdd(name, type);
+
 
     console.log('Done creating widget component %s.', name);
 };
