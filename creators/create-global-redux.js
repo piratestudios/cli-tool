f49c4d427c7302d10f0b228a547b2fff292bc2ad
Alvaro Torpedo <goodbytes23@yahoo.es>
2017-05-23 17:14:08 +0200

    Global redux duck

--- creators/create-global-redux.js
+++ creators/create-global-redux.js
@@ -1,12 +1,20 @@
 var createDirectory = require('./fs').createDirectory;
+var createFile = require('./fs').createFile;
 var createFiles = require('./fs').createFiles;
 
 module.exports = function (name) {
     console.log('Creating global redux component %s...', name);
 
     const
-        type = 'global-redux',
+        fileTemplate = require('../templates/templateDuck.js')(name),
+        type = 'redux',
         extensions = [];
+
+    extensions.push('.js');
+
+    createDirectory(type);
+    createDirectory(type + "/" + name);
+    createFiles(extensions, name, type, fileTemplate);
 
     console.log('Done creating global redux %s.', name);
 };
---
+++ src/redux/exaple/exaple.redux.js
@@ -0,0 +1,9 @@
+
+// ===============================================
+// Style Guide : we're trying out airbnb's ethos of only using redux for router transitions and network interactions
+// so try and use this file for only those if possible and keep rest state storage using local state.
+// see Getting Surgical and Philosophical about State at : https://medium.com/airbnb-engineering/rearchitecting-airbnbs-frontend-5e213efc24d2
+// ===============================================
+
+// see https://github.com/erikras/ducks-modular-redux for how to structure this and naming conventions of the actions
+    
